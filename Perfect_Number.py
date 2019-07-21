f19151bc2c45bf9bebbf1ff8ad61d68a5806b9a2
devu-62442 <50922916+devu-62442@users.noreply.github.com>
2019-07-21 19:15:56 +0530

    Update Perfect_Number.py

--- Perfect_Number.py
+++ Perfect_Number.py
@@ -1,6 +1,13 @@
-#Aliquot sum
-#Perfect Number
+# Perfect Number
+# Aliquot Sum (another name)
 
+# -----------------------------------------------------------------------------------------------------------------------------
+# A number is called perfect if it is the sum of its proper positive divisors. 
+# For example, 6 is perfect, because its divisors 1,2,3 add up to 6. 
+# Write a function to return True if a given number n is perfect. Otherwise return False.
+# -----------------------------------------------------------------------------------------------------------------------------
+
+# Method which determines whether the given number is a PERFECT NUMBER or NOT
 def Aliquot(a):
     s=0
     for i in range(1,a):
@@ -11,5 +18,7 @@ def Aliquot(a):
     else:
         print("Not a PERFECT NUMBER:"+(str)(a))
         
+# Getting the input from the user
 a=(int)(input("Enter the number to ckeck whether it is PERFECT or NOT: "))
+# Calling the Method
 Aliquot(a)
