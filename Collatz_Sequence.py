4cda99d02612cf56c01e8fff8a29f84c5b4dbcf8
devu-62442 <50922916+devu-62442@users.noreply.github.com>
2019-07-21 19:30:50 +0530

    Update Collatz_Sequence.py

--- Collatz_Sequence.py
+++ Collatz_Sequence.py
@@ -1,4 +1,13 @@
 #Collatz Sequence
+# -----------------------------------------------------------------------------------------------------------------------------
+# Given N, apply the following rules iteratively to get a sequence that ends with 1.
+# N→N/2 (if N is even)
+# N→(3∗N)+1 (if N is odd)
+# For example:
+# 13 → 40 → 20 → 10 → 5 → 16 → 8 → 4 → 2 → 1
+# Write a function to generate a Collatz sequence. 
+#-----------------------------------------------------------------------------------------------------------------------------
+# Method to generate the COLLATZ SEQUENCE
 def Collatz(a):
     b=a
     i=1
@@ -15,9 +24,10 @@ def Collatz(a):
     
     for i in range(0,len(list1)):
         print(list1[i]," -> ",end="")
-    
+
+# Getting the input from the user
 a=(int)(input("Enter the number to start generating Collatz Sequence:"))
-if(a<1000000):
-    Collatz(a)
-else:
-    print("The Number Is GREATER Than 999999")
+# If you input 1 million you will get the longest Collatz Sequence
+
+# Calling the method
+Collatz(a)
